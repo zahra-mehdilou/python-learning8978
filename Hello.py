dc8107da38d4ba6094108de2268aa754dd00cf2a
zahra-mehdilou <mehdilouzahra@gmail.com>
2025-12-31 00:51:27 +0330

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,17 @@
+a=18
+b=12
+gcd=1
+n=2
+while n<=min(a,b):
+    if a%n==0 and b%n==0:
+        gcd=gcd*n
+        a=a//n
+        b=b//n
+    else:
+        n=n+1
+print("GCD is",gcd)
+
+
+
+        
+
