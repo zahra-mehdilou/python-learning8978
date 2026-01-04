f65606c3c7a8aa778b2168c1f4bee855f6e06bc6
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-04 23:54:20 +0330

    Update students.py

--- students.py
+++ students.py
@@ -2,17 +2,32 @@ number=int(input("Enter the number of students:"))
 print("And now for each student first enter the full name and then enter the score:")
 students={}
 for i in range(number):
-    name=input("Enter the name:")
-    score=int(input(f"Enter the score of {name}: "))
-    while 0>score or score>20 :
-        print("The score is not valid!")
-        score=int(input(f"Enter the valid score of {name}: "))
-    students[name]=score
+    name=input("Enter the name: ")
+    char_length=len(name)
+    while char_length<3 or char_length>30:
+        print("❗️Your entered name is not valid! The number of characters should be between 3 and 30!")
+        name=input("Enter a valid name: ")
+        char_length=len(name)
+    score=float(input(f"Enter the score of {name}: "))
+    while score<0 or score>20 :
+        print("❗️The score is not valid!")
+        score=float(input(f"Enter the valid score of {name}: "))
+    if i==0:          
+        max_score=score
+        max_name=name
+        min_score=score
+        min_name=name
+    else:
+        if max_score<=score:
+            max_score=score
+            max_name=name
+        if min_score>=score:
+            min_score=score
+            min_name=name
+    students[name]=score 
 print(students)
-max_score=max(students, key=students.get)
-print (f" 😇 {students[max_score]} is the highest score and belongs to {max_score} ")
-min_score=min(students, key=students.get)
-print (f" ☹️ {students[min_score]} is the lowest score and belongs to {min_score} ")
+print(f"'{max_score}' is the highest score and belongs to '{max_name}' ")
+print(f"'{min_score}; is the lowest score and belongs to '{min_name}' ")
 while True:
  name_score=(input("Enter the name you want to know their score:"))
  if name_score == "exit" :
@@ -21,3 +36,4 @@ while True:
      print(f"{name_score}'s score is {students[name_score]}")
  else:
    print("This name is not in the list")
+
