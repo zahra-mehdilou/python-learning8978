f7d6136854369dd4c38ddec5fb30aa987067ec52
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-06 00:08:03 +0330

    Update students.py

--- students.py
+++ students.py
@@ -12,22 +12,25 @@ for i in range(number):
     while score<0 or score>20 :
         print("❗️The score is not valid!")
         score=float(input(f"Enter the valid score of {name}: "))
-    if i==0:          
+    students[name]=score 
+first=True
+for name,score in students.items():
+    if first==True:
         max_score=score
         max_name=name
         min_score=score
         min_name=name
+        first=False
     else:
         if max_score<=score:
-            max_score=score
-            max_name=name
+           max_score=score
+           max_name=name
         if min_score>=score:
-            min_score=score
-            min_name=name
-    students[name]=score 
+           min_score=score
+           min_name=name
 print(students)
-print(f"'{max_score}' is the highest score and belongs to '{max_name}' ")
-print(f"'{min_score}; is the lowest score and belongs to '{min_name}' ")
+print(f"😍 The highest score is {max_score} and belongs to {max_name} ")
+print(f"😒 The lowest score is {min_score} and belongs to {min_name} ")
 while True:
  name_score=(input("Enter the name you want to know their score:"))
  if name_score == "exit" :
@@ -36,4 +39,5 @@ while True:
      print(f"{name_score}'s score is {students[name_score]}")
  else:
    print("This name is not in the list")
+
 
