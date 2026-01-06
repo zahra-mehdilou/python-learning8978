c0373eba1a40e6d3cd308a8b87b747943c533f3c
zahra-mehdilou <mehdilouzahra@gmail.com>
2026-01-07 00:42:48 +0330

    Update revers.py

--- revers.py
+++ revers.py
@@ -3,7 +3,6 @@ def reverse(word):
     new_word=""
     for i in range(l-1,-1,-1):
             new_word+=word[i]
-    return(new_word)
+    print(new_word)
 word=str(input("Enter a word:"))
-a=reverse(word)
-print(a)
+reverse(word)
